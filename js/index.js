0187895e56c35ead143cb6776198eb9873a3e750
lelopez02 <lop20027@byui.edu>
2022-04-27 00:08:25 -0300

    adds css style

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-const date = new Date()
-console.log(date.getFullYear()) 
-let year = document.querySelector(".year")
-year.textContent = date.getFullYear()
+   
+// get the Current Date
+const today = new Date();
 
-const day = date.getDate()
-console.log(day)
+// get the day, month and year
+const day = today.getDate();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
+
+// get the hours, minutes and seconds
+const hours = today.getHours();
+const minutes = today.getMinutes();
+const seconds = today.getSeconds();
+
+const modified = document.lastModified;
+
+// takes the last-updated class from html
+const lastUpdated = document.querySelector(".last-updated");
+
+document.getElementById("date-and-time").textContent = year;
+
+lastUpdated.textContent = `Last Updated ${modified}`;
