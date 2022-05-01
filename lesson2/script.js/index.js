4efbb14a803b0a2777919978a67f1c3cb7555d50
lelopez02 <lop20027@byui.edu>
2022-05-01 00:28:49 -0300

    adds css styles

--- lesson2/script.js/index.js
+++
@@ -1,8 +0,0 @@
-const today = new Date();
-const year = today.getFullYear();
-const modified = document.lastModified;
-const lastUpdated = document.querySelector(".last-updated");
-
-document.getElementById("date-and-time").textContent = year;
-
-lastUpdated.textContent = `Last Updated ${modified}`;
