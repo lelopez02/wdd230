9db0b5357fae0286dc1607dcb8a527a27c113e86
lelopez02 <lop20027@byui.edu>
2023-04-27 22:08:13 -0300

    adds js code lines

---
+++ js/script.js
@@ -0,0 +1,12 @@
+try {
+    let options = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+    };
+  
+    const currentDateTime = new Date().toLocaleDateString("en-AR", options) + " " + new Date().toLocaleTimeString();
+    document.getElementById("currentdatetime").textContent = currentDateTime;
+  } catch (e) {
+    console.error(e);
+  }
