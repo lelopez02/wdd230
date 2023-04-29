0fbb90088f0c439e9cb5b3b28d8747f54d38d83c
lelopez02 <lop20027@byui.edu>
2023-04-29 19:43:55 -0300

    adds new code, images for lesson2

---
+++ lesson2/js/script.js
@@ -0,0 +1,16 @@
+
+try {
+    let options = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+    };
+  
+    const currentDateTime = new Date().toLocaleDateString("en-AR", options) + " " + new Date().toLocaleTimeString();
+    document.getElementById("currentdatetime").textContent = currentDateTime;
+  } catch (e) {
+    console.error(e);
+  };
+
+
+  document.querySelector("#copyrightYear").textContent = new Date().getFullYear();
