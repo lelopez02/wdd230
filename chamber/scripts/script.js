89da41149fbece0b2ae3a54909d305dc7be660bc
lelopez02 <lop20027@byui.edu>
2023-05-13 17:32:34 -0300

    adds css styles

---
+++ chamber/scripts/script.js
@@ -0,0 +1,36 @@
+
+
+// select the DOM elements to manipulate (we will output to these)
+const datefield = document.querySelector("time");
+// for european/family history format with day first.
+const datefieldUK = document.querySelector("aside");
+const datemessage = document.querySelector("p");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// "full" or long, medium, short options ... try them
+
+datefield.textContent = fulldate;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+datemessage.innerHTML = `Today is <strong>${fulldate}</strong>. What are you going to do?`;
+
+try {
+    let options = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+    };
+  
+    const currentDateTime = new Date().toLocaleDateString("en-AR", options) + " " + new Date().toLocaleTimeString();
+    document.getElementById("currentdatetime").textContent = currentDateTime;
+  } catch (e) {
+    console.error(e);
+  };
+  let date = new Date();
+  document.getElementById("#copyrightYear").textContent = date.getFullYear();
