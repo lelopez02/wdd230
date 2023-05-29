18f265fd8400818192c2d434b52cf7e71001f5df
lelopez02 <lop20027@byui.edu>
2023-05-29 09:03:29 -0300

    fixing errors

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -5,62 +5,57 @@ const navi = document.querySelector('.navi');
 
 
 hamButton.addEventListener('click', () => {
-	navi.classList.toggle("responsive");
+  navi.classList.toggle("responsive");
 });
 
- //current date on header
+//current date on header
 
- const datefieldUK = document.querySelector("aside");
- const datemessage = document.querySelector("p");
- 
- 
- const now = new Date();
- const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-   now
- );
- const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-   dateStyle: "full"
- }).format(now);
- 
+const datefieldUK = document.querySelector("aside");
+const datemessage = document.querySelector("p");
+
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "full"
+}).format(
+  now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full"
+}).format(now);
+
+//BANNER code
+
+const banner = document.querySelector("#hide");
+console.log(new Date().getDay());
+
+switch (new Date().getDay()) {
+  case 1:
+  case 2:
+    banner.classList.toggle("banner-hide");
+}
+
 
 //current date on footer
 try {
-    let options = {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-    };
-  
-    const currentDateTime = new Date().toLocaleDateString("en-AR", options) + " " + new Date().toLocaleTimeString();
-    document.getElementById("currentdatetime").textContent = currentDateTime;
-  } catch (e) {
-    console.error(e);
+  let options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
   };
 
-  //copyright on footer
-  let date = new Date();
-  document.getElementById("#copyrightYear").textContent = date.getFullYear();
+  const currentDateTime = new Date().toLocaleDateString("en-AR", options) + " " + new Date().toLocaleTimeString();
+  document.getElementById("currentdatetime").textContent = currentDateTime;
+} catch (e) {
+  console.error(e);
+};
+
+//copyright on footer
+let date = new Date();
+document.getElementById("#copyrightYear").textContent = date.getFullYear();
 
 
-  // "full" or long, medium, short options ... try them
-  
-  
-  datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+// "full" or long, medium, short options ... try them
 
-  //BANNER code
 
-  window.onresize = () => {
-    if (window.innerWidth > 600) mainnav.classList.remove("responsive");
-  };
-  
-  const banner = () => {
-    document.querySelector(".hide").style.display = "block";
-  };
-  
-  
-  if (dayNumber === 1 || dayNumber === 2) {
-    banner();
-  }
-  
-  console.log (dayNumber);
-  
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
