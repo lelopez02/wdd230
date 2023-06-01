66a795a7dbfb026e19246b1a55b0e2f43c1c6252
lelopez02 <lop20027@byui.edu>
2023-06-01 00:49:55 -0300

    adding alt tag

---
+++ lesson-7/js/script.js
@@ -0,0 +1,27 @@
+const imgOptions = {};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesDataSrc.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesDataSrc.forEach((img) => {
+        loadImages(img);
+    });
+}
