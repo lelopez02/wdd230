610f202c4750b88d27e9c4497833da9c01cebbdd
lelopez02 <lop20027@byui.edu>
2023-06-29 22:59:04 -0300

    fixin errors

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,22 +1,3 @@
-//function calcWindChill(t, s) {
-    //if (t <= 50 && s > 3){
-        //const wind_chill = Math.round(
-        //    35.74 + 0.6215 * t - 35.75 * s **0.16 + 0.4275 * t * s ** 0.16);
-      //  return wind_chill;
-    //} else {
-    //    return "N/A";
-   // };
- // };
-    
- // let s = document.querySelector(".wind-speed").textContent;
-  
- // document.querySelector(".wind-chill").textContent = 
- //   "Wind Chill: "+calcWindChill(t, s)+ "°F";
-  
- // document.querySelector(".temp").textContent = t+ "°F";
-  
- // document.querySelector(".wind-speed").textContent = "Wind Speed: "+s+" MPH";
-
  const currentTemp = document.querySelector('#current-temp');
  const windSpeedElement = document.querySelector('#wind-speed');
  const feelsLikeTemperatureElement = document.querySelector('#feels-like-temperature');
